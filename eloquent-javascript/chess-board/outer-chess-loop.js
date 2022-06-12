f3c92e769ed4c61e57b73754969e57d96e848946
= <mark.matanza@gmail.com>
2022-06-11 17:52:34 -0700

    more chess program tidbits

---
+++ eloquent-javascript/chess-board/outer-chess-loop.js
@@ -0,0 +1,19 @@
+// A simplified program
+// for constructing the
+// 8 x 8 chessboard
+// Outer loop:
+
+
+let grid = 8;
+
+let board = '';
+
+for (let x = 0; x < grid; x++) {
+
+  board += x +" #";
+
+  board += "\n";
+
+  console.log(board);
+
+}
