d58ab9b14ad5c64eae69b1b8694569ffb9e1b86c
= <mark.matanza@gmail.com>
2022-06-29 15:21:05 -0700

    complete php crash course

--- php/traversy-media/php-crash-course-2022/feedback/index.php
+++ php/traversy-media/php-crash-course-2022/feedback/index.php
@@ -1,5 +1,52 @@
 <?php include 'inc/header.php'; ?>
 
+<?php 
+
+$name = $email = $body = '';
+$nameErr = $emailErr = $bodyErr = '';
+
+if(isset($_POST['submit'])) {
+
+  // validate name
+  if(empty($_POST['name'])) {
+    $nameErr = 'Name is required';
+  } else {
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  }
+
+  // validate email
+  if(empty($_POST['email'])) {
+    $emailErr = 'Email is required';
+  } else {
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+  }
+  
+  // validate feedback/body
+  if(empty($_POST['body'])) {
+    $bodyErr = 'Feedback is required';
+  } else {
+    $body = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  }  
+
+  if(empty($nameErr) && empty($emailErr) && empty($bodyErr)) {
+
+    // Add to database
+    $sql = "INSERT INTO feedback (name, email, body) VALUES ('$name', '$email', '$body')";
+
+    if(mysqli_query($conn, $sql)) {
+      // Success
+      header("Location: /php/traversy-media/php-crash-course-2022/feedback/feedback.php");
+    } else {
+      // Error
+      echo 'Error: ' . mysqli_error($conn);
+
+    }
+
+  }
+
+}
+
+?>
 
     <img src="./img/logo.png" class="w-25 mb-3" alt="">
     <h2>Feedback</h2>
@@ -11,18 +58,24 @@
     ); ?>" class="mt-4 w-75">
       <div class="mb-3">
         <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name" value="">
-        <div id="validationServerFeedback" class="invalid-feedback">
-          Please provide a valid name.
+        <input type="text" class="form-control <?php echo !$nameErr ? 'is-invalid' : null; ?>" id="name" name="name" placeholder="Enter your name" value="">
+        <div class="invalid-feedback">  
+          <?php echo $nameErr; ?>
         </div>
       </div>
       <div class="mb-3">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email" value="">
+        <input type="email" class="form-control <?php echo !$emailErr ? 'is-invalid' : null; ?>" id="email" name="email" placeholder="Enter your email" value="">
+        <div class="invalid-feedback">  
+          <?php echo $emailErr; ?>
+        </div>        
       </div>
       <div class="mb-3">
         <label for="body" class="form-label">Feedback</label>
-        <textarea class="form-control" id="body" name="body" placeholder="Enter your feedback"></textarea>
+        <textarea class="form-control <?php echo !$bodyErr ? 'is-invalid' : null; ?>" id="body" name="body" placeholder="Enter your feedback"></textarea>
+        <div class="invalid-feedback">  
+          <?php echo $bodyErr; ?>
+        </div>
       </div>
       <div class="mb-3">
         <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
