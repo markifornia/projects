6deac568e2802f9b75837ed6c180808139303878
= <mark.matanza@gmail.com>
2022-07-02 19:00:40 -0700

    rework recursive puzzle

--- eloquent-javascript/math-formulas/recursive-puzzle.js
+++ eloquent-javascript/math-formulas/recursive-puzzle.js
@@ -10,18 +10,38 @@ tries to find a sequence of such additions and multiplications that produces tha
 function findSolution(target) {
     
     function find(current, history) {
-        if (current, history) {
+
+        if (current == target) {
             return history;
-        } else if (target == current) {
+        } else if (current > target) {
             return null;
         } else {
-            return find(current + 5, `${history} + 5`) || find(current * 3, `${history} * 3`);
+            return find(current + 5, `(${history} + 5)`) || find(current * 3, `(${history} * 3)`);
+        
         }
     }
 
     return find(1, "1");
 }
 
-//findSolution(24);
+/*
+* (1) If the current number is the target number, the current history
+* is way to reach the target, so this history is kept for the next 
+* subequent recursion.
+*
+* (2) If the current number is greater thatn the target, there is no
+* sense in continuing, so we return null.
+*
+* (3) Finally, if we're still below the target number, the function tries
+* both possible paths using (||)
+*/
 
-console.log(findSolution(24));
+//console.log(findSolution(24)); // (((1 * 3) + 5) * 3)
+
+// Let's try 100
+//console.log(findSolution(100)); // we get null
+
+// Let's try 48
+console.log(findSolution(48)); // ((((1 + 5) + 5) + 5) * 3)
+
+// Conclusion - This exercise takes a bit of time to understand, basically when the arithmetic is run through the function each time to get close to the solution, its computation is housed in yet another set of parenthesis and then that operation is run till the solution is found. 
