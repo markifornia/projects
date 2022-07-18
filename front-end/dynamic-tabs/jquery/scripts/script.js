808898e77edbce11e8ac77fce8f1dd0559c85ce3
= <mark.matanza@gmail.com>
2022-07-18 11:55:39 -0700

    fix: increase height on tab hover

--- front-end/dynamic-tabs/jquery/scripts/script.js
+++ front-end/dynamic-tabs/jquery/scripts/script.js
@@ -1,23 +1,42 @@
 /**
- * Jquery version 3.6.0
+ * jQuery version 3.6.0
  */
 
-const tabs = document.querySelectorAll('[data-tab-target]');
-const tabContents = document.querySelectorAll('[data-tab-content]');
+(function($) {
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.tabTarget);
+    $(document).ready(function() {
 
-        tabContents.forEach(tabContent => {
-            tabContent.classList.remove('active');
+        const tabs = document.querySelectorAll('[data-tab-target]');
+        const tabContents = document.querySelectorAll('[data-tab-content]');
+        
+        tabs.forEach(tab => {
+            tab.addEventListener('click', () => {
+                const target = document.querySelector(tab.dataset.tabTarget);
+        
+                tabContents.forEach(tabContent => {
+                    tabContent.classList.remove('active');
+                });
+        
+                tabs.forEach(tab => {
+                    tab.classList.remove('active');
+                });        
+        
+                tab.classList.add('active');
+                target.classList.add('active');
+            });
         });
 
-        tabs.forEach(tab => {
-            tab.classList.remove('active');
+        const boxHeight = $(".tab").height();
+        $(".tab").mouseenter(function(){
+            $(this).animate({
+                //paddingTop: "10px"
+            });
+        }).mouseleave(function(){
+            $(this).animate({
+                height: boxHeight
+            });
         });        
 
-        tab.classList.add('active');
-        target.classList.add('active');
     });
-});
+
+}(jQuery));
