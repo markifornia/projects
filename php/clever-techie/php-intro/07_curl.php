4e4157c447fcfba0a9e23b2cea24ccc9cf616dd2
= <mark.matanza@gmail.com>
2022-07-20 00:31:12 -0700

    07_curl.php loop through results

--- php/clever-techie/php-intro/07_curl.php
+++ php/clever-techie/php-intro/07_curl.php
@@ -13,7 +13,31 @@
 * curl_close($curl);
 */
 
-$curl = curl_init(); // curl is going to be daa type curl resource
-curl_setopt($curl, CURLOPT_URL, 'https://www.amazon.com');
+$curl = curl_init(); // curl is going to be data type curl resource
+
+$search_string = "pc video games 2022";
+$url = "https://www.amazon.com/s?k=$search_string";
+
+curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-curl_exec($curl);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+// store result
+$result = curl_exec($curl);
+
+preg_match_all("/https://images-na.ssl-images-amazon.com/images/I/[^\s]*?._AC_USZ160_.jpg/", $result, $matches);
+
+// remove duplicates
+$images = array_unique($matces[0]);
+
+// reset keys
+$images = array_values($images);
+
+for ($i = 0; $i < count($images); $i++) {
+    echo "<div style='float: left; margin: 10 0 0 0; '>";
+    echo "<img src='$images[$i]'><br>";
+    echo "</div>";
+}
+
+// free up resources
+curl_close($curl);
