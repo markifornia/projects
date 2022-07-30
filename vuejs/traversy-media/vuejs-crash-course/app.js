e25ea11d91315f7d04a348c237990ac39de593b9
= <mark.matanza@gmail.com>
2022-07-29 23:31:08 -0700

    (37:10) task data

--- vuejs/traversy-media/vuejs-crash-course/app.js
+++ vuejs/traversy-media/vuejs-crash-course/app.js
@@ -17,9 +17,9 @@ const app  = Vue.createApp({
 
             this.firstName = results[0].name.first,
             this.lastName = results[0].name.last,
-            this.email = 'john@gmail.com',
-            this.gender = 'female',
-            this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
+            this.email = results[0].email,
+            this.gender = results[0].gender,
+            this.picture = results[0].picture.large
         }
     }
 });
