d5eecda358a3a50fbe9be3b8b145d76fed6b6f18
= <mark.matanza@gmail.com>
2022-07-31 21:53:15 -0700

    (46:04) emit events (delete task)

--- vuejs/traversy-media/vue-crash-2021/src/App.vue
+++ vuejs/traversy-media/vue-crash-2021/src/App.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="container">
     <Header title="Add Task" />
+    <Tasks :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  }, 
+  created() {
+    this.tasks = [
+      {
+        id: 1, 
+        text: 'Doctors Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'March 3rd at 1:30pm',
+        reminder: true
+      },
+      {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'March 3rd at 11:00pm',
+      reminder: false
+      }
+    ]
   }
 }
 </script>
---
+++ vuejs/traversy-media/vue-crash-2021/src/components/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <Task :task="task"></Task>
+    </div>
+</template>
+
+<script>
+import Task from './Task'
+
+    export default {
+        name: 'Tasks',
+        props: {
+            tasks: Array
+        }, 
+        components: {
+            Task
+        }
+    }
+</script>
