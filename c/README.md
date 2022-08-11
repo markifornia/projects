edb8481f077d8f22643820626a3161a5c48036b5
= <mark.matanza@gmail.com>
2022-08-11 01:03:26 -0700

    simple c programs

---
+++ c/README.md
@@ -0,0 +1,1 @@
+Some super simple C programs.
---
+++ c/learn.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+	10 == 10 ? printf("Correct, b1\n") : printf("Incorrect\n");
+	
+	if (10 == 10) {
+		printf("Correct\n");
+	} else {
+
+		printf("False\n");	
+	}
+	
+	return 0;
+
+}
